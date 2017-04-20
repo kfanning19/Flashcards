9ec74b1b9d16bc5b7f59b57146611823074d1d93
Kristen Fanning <kristen.fanning@northwestern.edu>
2017-04-20 16:34:35 -0500

    add instance of

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,10 +1,13 @@
 var ClozeCard = function(text, cloze){
 	if(!(text.includes(cloze))){
 		console.log("Error: " + cloze + " does not appear in " + text);
-	};
+	} else if (this instanceof ClozeCard){
 	this.cloze = cloze;
 	this.partial = text.replace(cloze, "...");
-	this.fulltext = text.replace(cloze, "*" + cloze + "*");
+	this.fullText = text;
+	} else{
+		return new ClozeCard(text, cloze);
+	}
 }
 
 module.exports = ClozeCard;
